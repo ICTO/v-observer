f38f36bfc9b9e05e71334a72e0639d88bb1674d9
Bruno Goossens <brunogoossens@gmail.com>
2016-02-03 16:59:34 +0100

    fixing styling issue

--- resources/views/observation/videoDisplay.blade.php
+++ resources/views/observation/videoDisplay.blade.php
@@ -4,13 +4,15 @@
 <div class="container">
     <div class="section">
         <div class="row">
-            <div class="col s12  l6">
+            <div class="col s12 l6 push-l3">
                 <div class="card left-align">
                     @include('observation.videos.'.$video->type.'.display', ['video' => $video, 'video_types' => $video_types, 'questionnaire' => $questionnaire])
                     @yield('video-actions')
                 </div>
             </div>
-            <div class="col s12  l6">
+        </div>
+        <div class="row">
+            <div class="col s12 l6 push-l3">
                 <div class="card left-align">
                     @section('video-analyses')
                     <div class="card-content">
