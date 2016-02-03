f7c7926c5d3277be5ea1fd27e2594fd08a89b7e4
Bruno Goossens <brunogoossens@gmail.com>
2016-02-03 17:11:36 +0100

    refixing styling issue

--- resources/views/observation/videoDisplay.blade.php
+++ resources/views/observation/videoDisplay.blade.php
@@ -4,15 +4,13 @@
 <div class="container">
     <div class="section">
         <div class="row">
-            <div class="col s12 l6 push-l3">
+            <div class="col s12 l6">
                 <div class="card left-align">
                     @include('observation.videos.'.$video->type.'.display', ['video' => $video, 'video_types' => $video_types, 'questionnaire' => $questionnaire])
                     @yield('video-actions')
                 </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col s12 l6 push-l3">
+            <div class="col s12 l6">
                 <div class="card left-align">
                     @section('video-analyses')
                     <div class="card-content">
